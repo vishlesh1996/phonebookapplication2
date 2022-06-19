4de289cec4e362bf4b167cc2a308ffff24fcb08f
vishlesh11996 <vishlesh.reakalwar@gmail.com>
2022-06-19 15:56:32 +0530

    PHN-1:create git hub repo

---
+++ src/main/java/com/BikkadIt/phonebookapplication/PhoneBookApplications3Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.phonebookapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookApplications3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookApplications3Application.class, args);
+	}
+
+}
